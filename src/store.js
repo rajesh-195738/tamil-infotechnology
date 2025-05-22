954de7ecbefe955a3521266e0ceffd74b5ab087f
rajesh-195738 <rajeshkannathiru10@gmail.com>
2025-05-22 16:17:32 +0530

    initial push

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loaderReducer from "./app/features/loaderSlice";
 
+// Store
 const store = configureStore({
     reducer:{
         loader: loaderReducer
